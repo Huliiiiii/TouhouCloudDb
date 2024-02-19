ec75b2505259f73b06c550a6a6c3a47463dc5504
Huli <308013446a@gmail.com>
2024-02-19 20:01:29 +0800

    添加发行搜索api

--- routes/api/search_api.js
+++ routes/api/search_api.js
@@ -1,32 +1,40 @@
 const router = require("express").Router();
-
 const { escapeId } = require("mysql2");
 const { pool } = require("../../functions/query.js");
 const { if_A_Return_B_else_C } = require("../../src/scripts/little_tools.js");
-
-function columnProcessor(target) {
-	return if_A_Return_B_else_C(target !== "*" && target !== "" && target !== undefined, escapeId(target), "*");
+/**
+ *
+ * @param {Array<String,Array<String>>} column
+ * @returns {String}
+ */
+function columnProcessor(column) {
+	// 接收到的是一个数组
+	let column_str = column.toString(column);
+	let column_arr = column_str.split(/,\s*/);
+	return if_A_Return_B_else_C(column !== "*" && column !== "" && column !== undefined, escapeId(column_arr), "*");
 }
 
 // 艺术家
 router.get("/api/search/artist", async (req, res) => {
 	try {
 		if (req.query.keyword) {
-			let column = columnProcessor(req.query.target);
+			// let column = columnProcessor(req.query.target);
+			let column = "*";
 			//
 			let keyword = req.query.keyword;
 			let name = `%${keyword}%`;
 			//
 			let query_statement = isNaN(keyword)
-				? `SELECT ${column} FROM artist WHERE name LIKE ? OR LOWER(aliases ->> "$") LIKE LOWER(?) AND is_deleted = 0`
-				: `SELECT ${column} FROM artist WHERE name LIKE ? OR LOWER(aliases ->> "$") LIKE LOWER(?) OR artist_id = ? AND is_deleted = 0`;
+				? `SELECT ${column} FROM artist WHERE name LIKE ? OR LOWER(alternative_name ->> "$") LIKE LOWER(?) AND is_deleted = 0`
+				: `SELECT ${column} FROM artist WHERE name LIKE ? OR LOWER(alternative_name ->> "$") LIKE LOWER(?) OR artist_id = ? AND is_deleted = 0`;
 			// eslint-disable-next-line no-unused-vars
 			const [result] = await pool.query(query_statement, [name, name, keyword]);
 			// console.log(pool.format(query_statement, [name, name, keyword]));
 			// console.log(result);
 			res.send(result);
 		} else if (req.query.id && !isNaN(parseFloat(req.query.id))) {
-			let column = columnProcessor(req.query.column);
+			// let column = columnProcessor(req.query.target);
+			let column = "*";
 			//
 			let id = req.query.id;
 			let query_statement = `SELECT ${column} FROM touhoudbtest.artist WHERE artist_id = ? AND is_deleted = 0 `;
@@ -46,31 +54,68 @@ router.get("/api/search/artist", async (req, res) => {
 router.get("/api/search/song", async (req, res) => {
 	try {
 		if (req.query.keyword) {
-			let column = columnProcessor(req.query.target);
+			let column = "*";
 			//
 			let keyword = req.query.keyword;
 			let title = `%${keyword}%`;
 			//
 			let query_statement = isNaN(keyword)
-				? `SELECT ${column} FROM touhoudbtest.song WHERE title LIKE ? OR AND is_deleted = 0`
+				? `SELECT ${column} FROM touhoudbtest.song WHERE title LIKE ? AND is_deleted = 0`
 				: `SELECT ${column} FROM touhoudbtest.song WHERE title LIKE ? OR song_id = ? AND is_deleted = 0`;
 			// eslint-disable-next-line no-unused-vars
+			// console.log(pool.format(query_statement, [title, keyword]));
 			const [result] = await pool.query(query_statement, [title, keyword]);
+			// console.log(result);
 			res.send(result);
 		} else if (req.query.id && !isNaN(parseFloat(req.query.id))) {
-			let column = columnProcessor(req.query.column);
+			let column = "*";
 			//
 			let id = req.query.id;
 			let query_statement = `SELECT ${column} FROM touhoudbtest.song WHERE song_id = ? AND is_deleted = 0`;
 			// eslint-disable-next-line no-unused-vars
 			const [result] = await pool.query(query_statement, id);
+			// console.log(pool.format(query_statement, id));
 			res.send(result);
 		} else {
 			res.status(500).send(Error("Invalid request"));
 		}
 	} catch (error) {
 		console.error("Error executing the query:", error);
-		res.status(500).send("Error while executing the query");
+		res.status(500).send(Error("Error while executing the query"));
+	}
+});
+// 发行
+router.get("/api/search/release", async (req, res) => {
+	try {
+		if (req.query.keyword) {
+			let column = "*";
+			//
+			let keyword = req.query.keyword;
+			let title = `%${keyword}%`;
+			//
+			let query_statement = isNaN(keyword)
+				? `SELECT ${column} FROM touhoudbtest.release WHERE title LIKE ? AND is_deleted = 0`
+				: `SELECT ${column} FROM touhoudbtest.release WHERE title LIKE ? OR release_id = ? AND is_deleted = 0`;
+			// eslint-disable-next-line no-unused-vars
+			// console.log(pool.format(query_statement, [title, keyword]));
+			const [result] = await pool.query(query_statement, [title, keyword]);
+			// console.log(result);
+			res.send(result);
+		} else if (req.query.id && !isNaN(parseFloat(req.query.id))) {
+			let column = "*";
+			//
+			let id = req.query.id;
+			let query_statement = `SELECT ${column} FROM touhoudbtest.release WHERE release_id = ? AND is_deleted = 0`;
+			// eslint-disable-next-line no-unused-vars
+			const [result] = await pool.query(query_statement, id);
+			// console.log(pool.format(query_statement, id));
+			res.send(result);
+		} else {
+			res.status(500).send(Error("Invalid request"));
+		}
+	} catch (error) {
+		console.error("Error executing the query:", error);
+		res.status(500).send(Error("Error while executing the query"));
 	}
 });
 
