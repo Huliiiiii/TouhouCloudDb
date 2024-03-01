a2286cc0e6c7a6fc4d820f7f9745e743f0c0fea0
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-03-02 04:09:08 +0800

    refactor(config): 将数据库密码转换为环境变量，并将其放入本地的.env.local文件中。
    fix(config.js): 修复Config.js使用ES Module时无法启动服务器的错误
    
    chore: 移除logs文件夹，并将其添加进gitignore，并移除不使用的index.html

--- server/config/config.d.ts
+++ server/config/config.d.ts
@@ -1,16 +1,22 @@
-namespace config {
-	export interface database {
-		host: string;
-		username: string;
-		password: string;
-		database_name: string;
-	}
-	export interface server {
-		port: number;
-	}
-	let database: database;
-	let server: server;
-	let log_dir:string;
+// config.d.ts
+
+interface DatabaseConfig {
+	host: string;
+	username: string;
+	password: string;
+	database_name: string;
 }
+
+interface ServerConfig {
+	port: number;
+}
+
+interface Config {
+	database: DatabaseConfig;
+	server: ServerConfig;
+	log_dir: string;
+}
+
+declare const config: Config;
 
 export default config;
--- server/config/config.js
+++ server/config/config.js
@@ -1,21 +1,21 @@
-require("dotenv").config();
-const path = require('path');
-const config = {
-    development: {
-        database: {
-            host: "localhost",
-            username: "root",
-            password: "zjy06282109",
-            database_name: "touhoudbtest",
-        },
-        server: {
-            port: 3007,
-        },
-        log_dir: path.join(__dirname, "..", "logs"),
-    },
-    test: {},
-    production: {}
+const path = require("path");
+require("dotenv").config({ path: [".env.local", ".env"] });
+
+const cfg = {
+	development: {
+		database: {
+			host: "localhost",
+			username: "root",
+			password: process.env.DATABASE_PASSWORD,
+			database_name: "touhoudbtest",
+		},
+		server: {
+			port: 3007,
+		},
+		log_dir: path.join(__dirname, "..", "logs"),
+	},
+	test: {},
+	production: {},
 };
-
-module.exports = config[process.env.NODE_ENV || "development"];
-
+const config = cfg[process.env.NODE_ENV || "development"];
+module.exports = config;
