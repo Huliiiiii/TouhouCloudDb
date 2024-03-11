5132f82bd544307c1ee47323e76a1cbfad638ada
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-03-11 21:08:44 +0800

    refactor: move route config to a separate file

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,87 +1,37 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 import { Route, Router } from "@solidjs/router";
-import { lazy, ErrorBoundary } from "solid-js";
-import "./index.css";
+import { ErrorBoundary } from "solid-js";
 
-const routes = [
-	{
-		path: "/",
-		component: lazy(() => import("/src/homepage"))
-	},
-	{
-		path: "/add",
-		children: [
-			{
-				path: "/release",
-				component: lazy(() => import("/src/routes/add/add_release"))
-			}
-		]
-	},
-	{
-		path: "/update",
-		children: [
-			{
-				path: "/release/:ID",
-				component: lazy(() => import("/src/routes/add/add_release"))
-			}
-		]
-	},
-	{
-		path: "/list",
-		children: [
-			{
-				path: "/albums",
-				component: lazy(() => import("/src/routes/list/AlbumList"))
-			}
-		]
-	},
-	{
-		path: "/dev",
-		children: [
-			{
-				path: "/edit/release/:id?",
-				component: lazy(() => import("/src/routes/dev/edit_release"))
-			},
-			{
-				path: "/edit/artist/:id?",
-				component: lazy(() => import("/src/routes/dev/edit_artist"))
-			},
-			{
-				path: "/edit/song/:id?",
-				component: lazy(() => import("/src/routes/dev/edit_song"))
-			},
-			{
-				path: "/list/song",
-				component: lazy(() => import("/src/routes/dev/list_song"))
-			},
-			// {
-			// 	path: "/test",
-			// 	component: lazy(() => import("/src/routes/dev/test"))
-			// }
-		]
-	}
-];
+import { Header } from "compoments/Headder.tsx";
+import { route_config } from "src/route_config";
+import "./index.css";
+import "./app.css";
 
 function App() {
 	return (
 		<>
-			<ErrorBoundary fallback={(err) => err}>
-				<div style={{ height: "10%" }}>
-					<button
-						onClick={() => {
-							window.location.href = "/";
-						}}
-					>
-						return
-					</button>
-				</div>
-				<Router>
-					<Route>{routes}</Route>
-					<Route path="*" component={() => <div>404 Not Found</div>} />
-				</Router>
-			</ErrorBoundary>
+			<div class="flex justify-center">
+				<ErrorBoundary fallback={(err) => err}>
+					<Router>
+						<Route>{route_config}</Route>
+						<Route path="*" component={() => <div>404 Not Found</div>} />
+					</Router>
+				</ErrorBoundary>
+			</div>
 		</>
 	);
 }
-render(App, document.getElementById("root")!);
+
+const Root = () => {
+	return (
+		<>
+			<div class="">
+				<Header />
+				<App />
+			</div>
+		</>
+	);
+};
+
+render(Root, document.getElementById("root")!);
---
+++ client/src/route_config.ts
@@ -0,0 +1,60 @@
+import { lazy } from "solid-js";
+
+export const route_config: any = [
+	{
+		path: "/",
+		component: lazy(() => import("src/homepage"))
+	},
+	{
+		path: "/add",
+		children: [
+			{
+				path: "/release",
+				component: lazy(() => import("routes/add/add_release"))
+			}
+		]
+	},
+	{
+		path: "/update",
+		children: [
+			{
+				path: "/release/:ID",
+				component: lazy(() => import("routes/add/add_release"))
+			}
+		]
+	},
+	{
+		path: "/list",
+		children: [
+			{
+				path: "/albums",
+				component: lazy(() => import("routes/list/AlbumList"))
+			}
+		]
+	},
+	{
+		path: "/dev",
+		children: [
+			{
+				path: "/edit/release/:id?",
+				component: lazy(() => import("routes/dev/edit_release"))
+			},
+			{
+				path: "/edit/artist/:id?",
+				component: lazy(() => import("routes/dev/edit_artist"))
+			},
+			{
+				path: "/edit/song/:id?",
+				component: lazy(() => import("routes/dev/edit_song/edit_song.tsx"))
+			},
+			{
+				path: "/list/song",
+				component: lazy(() => import("routes/dev/list_song"))
+			},
+			{
+				path: "/test",
+				component: lazy(() => import("routes/dev/test"))
+			}
+		]
+	}
+];
