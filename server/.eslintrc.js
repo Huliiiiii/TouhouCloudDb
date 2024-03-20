e7e966b73e69bde30bd835ecebd84709f0dba388
狐狸 <134658521+Huliiiiii@users.noreply.github.com>
2024-03-20 12:37:50 +0800

    chore: update eslint config

---
+++ server/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+	env: {
+		commonjs: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ["eslint:recommended", "prettier"],
+	parserOptions: {
+		ecmaVersion: "latest",
+	},
+	rules: {},
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+		{
+			env: { node: true, es2021: true },
+			files: ["*.ts"],
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				project: "./tsconfig.json",
+				ecmaVersion: "latest",
+				sourceType: "module",
+			},
+			extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended-type-checked", "plugin:@typescript-eslint/stylistic-type-checked", "prettier"],
+			rules: {
+				"@typescript-eslint/no-misused-promises": ["error", { checksVoidReturn: false }],
+			},
+		},
+	],
+};
